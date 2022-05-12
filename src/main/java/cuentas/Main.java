5be7cf5164d9d43a105b6de5f5af5eefcc664113
PaquitoElbananin <102551124+PaquitoElbananin@users.noreply.github.com>
2022-05-12 18:56:36 +0100

    Update Main.java

--- src/main/java/cuentas/Main.java
+++ src/main/java/cuentas/Main.java
@@ -2,6 +2,6 @@ package cuentas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       operativa_cuenta.run( int operacion, float cantidad);
     }
 }
